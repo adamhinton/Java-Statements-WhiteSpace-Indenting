8cc9ad6ddd252dbd47b751d54821e3a959e83305
adamd <adam.d.hinton@gmail.com>
2024-02-11 10:30:38 -0500

    dummy variable

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("Blah blah blah");
     }
 }
